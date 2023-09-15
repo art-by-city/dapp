3f32625611ae2808d278c813a21fa4ee11b37993
Jim Toth <jim@artby.city>
2023-09-15 14:55:01 -0400

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
   <div style="height:200px">
-    your art forever
+    <h1>your art forever</h1>
+
+    <v-btn @click="refresh">refresh</v-btn>
+
+    <v-container v-if="data">
+      <v-row v-for="publication in data.publications" :key="publication.id">
+        {{ publication }}
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script setup lang="ts">
+const abc = useArtByCity()
+
+
+
+const { data, refresh } = useLazyAsyncData('publications', async () => {
+  const { publications } = await abc.legacy.queryPublications(10)
+
+  console.log('got publications!', publications)
+
+  return { publications }
+})
+</script>
