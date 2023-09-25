3c5b072fee68aa06ce245e15714efca4eb94aab8
Nopfed <robby.tsunami@gmail.com>
2023-09-25 18:12:14 -0400

    WIP: Adds hover fade transition and pending load bar

--- components/FeedItemCard.vue
+++ components/FeedItemCard.vue
@@ -1,16 +1,55 @@
 <template>
+  <v-container fluid class="fill-height" v-if="pending">
+    <v-row class="ma-0">
+      <v-col
+        cols="12"
+        class="pending font-weight-md-thin scale-text text-center"
+      >
+        {{ data?.id }}
+      </v-col>
+      <v-col cols="12">
+        <v-progress-linear
+          indeterminate
+          color="black"
+          background-color="transparent"
+          height="1"
+          bottom
+        ></v-progress-linear>
+      </v-col>
+    </v-row>
+  </v-container>
   <v-img
+    v-show="!pending"
     :src="'https://arweave.net/' + data?.images[0].preview"
     aspect-ratio="1"
     @click="goToArt"
     @click.middle="newTabArt"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
   >
+    <Transition name="fade">
+      <div v-if="hover" class="hover">
+        <v-container>
+          <v-row >
+            <v-col>
+              <a class="white--text font-weight-bold">{{ data?.title }}</a>
+              <br />
+              <a class="white--text font-italic">
+                {{ data?.creator }}
+              </a>
+            </v-col>
+          </v-row>
+        </v-container>
+        
+      </div>
+    </Transition>
   </v-img>
 </template>
 
 
 <script setup lang="ts">
 
+const hover = ref(false)
 const { id } = defineProps<{ id: string }>()
 const abc = useArtByCity()
 
@@ -32,6 +71,23 @@ function newTabArt() {
 
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.25s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
+.pending{
+  color: rgb(38, 38, 38);
+  font-size: large;
+}
+
+.hover {
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 100%;
+}
 </style>
