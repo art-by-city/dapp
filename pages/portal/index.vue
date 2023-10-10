1482680f63e0adb0a051b0c007643a988984a54f
Jim Toth <jim@artby.city>
2023-10-10 12:17:21 -0400

    Adds QRCode component

---
+++ pages/portal/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <QRCode :text="qrText" />
+  <v-text-field v-model="text" />
+  <v-btn @click="onGenerateClicked">generate</v-btn>
+</template>
+
+<script setup lang="ts">
+const text = ref('')
+const qrText = ref('')
+const onGenerateClicked = debounce(() => qrText.value = text.value)
+</script>
