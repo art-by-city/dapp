b7e94e7bb1e82fbf74ba76a2c142a9e8eb108404
Nopfed <robby.tsunami@gmail.com>
2023-10-13 19:10:05 -0400

    WIP: Adds more to bio and custom collapsible text component

--- components/BioCard.vue
+++ components/BioCard.vue
@@ -1,20 +1,75 @@
 <template>
   <v-card elevation="0">
-    <v-card-title>{{ address }}</v-card-title>
+    <v-card-title>{{ props.address }}</v-card-title>
     <v-card-subtitle class="pb-0">
       <p class="mb-0">
-        Second Name
+        @{{ profile?.displayName }}
       </p>
       <p class="mb-0">
-        Third Name
+        <v-img src="" class="px-4" />{{ props.address }}
       </p>
-      <p class="mb-0">
-        Socials
+      <p v-if="profile?.twitter" class="mb-0">
+        <v-icon color="#1da1f2" class="px-4 pb-1">
+          mdi-twitter
+        </v-icon>
+        <nuxt-link
+          :to="`https://twitter.com/${profile?.twitter}`"
+          target="_blank"
+          class="text-decoration-none"
+        >
+          {{ profile?.twitter }}
+        </nuxt-link>
+      </p>
+      <p v-if="profile?.instagram" class="mb-0">
+        <v-icon color="#d300c5" class="px-4 pb-1">
+          mdi-instagram
+        </v-icon>
+        <nuxt-link
+          :to="`https://instagram.com/${profile?.instagram}`"
+          target="_blank"
+          class="text-decoration-none"
+        >
+          {{ profile?.instagram || 'blah blah' }}
+        </nuxt-link>
+      </p>
+      <p v-if="profile?.soundcloud" class="mb-0">
+        <v-icon color="black" class="px-4 pb-1">
+          mdi-soundcloud
+        </v-icon>
+        <nuxt-link
+          :to="`https://soundcloud.com/${profile?.soundcloud}`"
+          target="_blank"
+          class="text-decoration-none"
+        >
+          {{ profile?.soundcloud }}
+        </nuxt-link>
+      </p>
+      <p v-if="profile?.twitch" class="mb-0">
+        <v-icon color="#9146FF" class="px-4 pb-1">
+          mdi-twitch
+        </v-icon>
+        <nuxt-link
+          :to="`https://twitch.tv/${profile?.twitch}`"
+          target="_blank"
+          class="text-decoration-none"
+        >
+          {{ profile?.twitch }}
+        </nuxt-link>
+      </p>
+      <p v-if="profile?.linkedin" class="mb-0">
+        <v-icon color="#0077b5" class="px-4 pb-1">
+          mdi-linkedin
+        </v-icon>
+        <nuxt-link
+          :to="`https://linkedin.com/${profile?.linkedin}`"
+          target="_blank"
+          class="text-decoration-none"
+        >
+          {{ profile?.linkedin }}
+        </nuxt-link>
       </p>
     </v-card-subtitle>
-    <v-card-text class="pb-0">
-      Artist bio goes here please.
-    </v-card-text>
+    <TextCollapsible :text="profile?.bio || ''" class="ma-4" />
   </v-card>
 </template>
 
@@ -24,6 +79,15 @@
 </style>
 
 <script setup lang="ts">
-defineProps<{ address: string }>()
+const props = defineProps<{ address: string }>()
+const abc = useArtByCity()
+
+const { 
+  data: profile 
+} = useLazyAsyncData(`profile-${props.address}`, async () => {
+  const profile = await abc.legacy.fetchProfile(props.address)
+
+  return profile
+})
 
 </script>
---
+++ components/TextCollapsible.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-expand-transition>
+      <p class="text-container" :class="{ collapsed }">
+        {{ text }}
+      </p>
+    </v-expand-transition>
+    <div class="text-center button-container" :class="{ collapsed }">
+      <v-btn
+        icon
+        size="x-small"
+        variant="plain"
+        @click="collapsed = !collapsed"
+      >
+        <v-icon>mdi-chevron-double-{{ collapsed ? 'down' : 'up' }}</v-icon>
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text-container.collapsed {
+  height: 3em;
+  overflow: hidden;
+}
+.button-container.collapsed::before {
+  content: "\00a0";
+  display: block;
+  margin-top: -15px;
+  position: relative;
+  bottom: 0;
+  width: 100%;
+  background:
+    linear-gradient(
+      to bottom,
+      rgba(255,255,255,0) 0%,
+      rgba(255,255,255,1) 50%,
+      rgba(255,255,255,1) 100%
+    )
+    left 
+    bottom
+    no-repeat;
+}
+</style>
+
+<script setup lang="ts">
+defineProps<{ text: string }>()
+
+const collapsed = ref<boolean>(true)
+</script>
