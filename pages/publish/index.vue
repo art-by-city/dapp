666eabd6bb5352cf61f0111fcdccde4434982988
Nopfed <robby.tsunami@gmail.com>
2023-11-29 15:49:01 -0500

    WIP: Canvas display image preview

--- pages/publish/index.vue
+++ pages/publish/index.vue
@@ -8,7 +8,7 @@
     <v-row>
       <v-col cols="6">      
         <v-row>
-          <v-col sm="6" class="pa-0 pt-sm-3 px-sm-3">
+          <v-col sm="11" class="pa-0 pt-sm-3 px-sm-3">
             <v-text-field variant="outlined" density="compact" label="Title" />
           </v-col>
         </v-row>
@@ -61,29 +61,33 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col>
-        <canvas id="canvas" ></canvas>
+
+      <v-col cols="6">
+        <canvas id="previewCanvas" width="500" height="500" style="border: 1px solid black;">
+          Your browser does not support the HTML 5 Canvas. 
+        </canvas>
       </v-col>
     </v-row>
-        
     
   </v-container>
 </template>
 
 <script setup lang="ts">
 const fileUpload = ref<File[]>([])
-
-const c = <HTMLCanvasElement> document.getElementById("canvas")
+const c = <HTMLCanvasElement> document.getElementById("previewCanvas")
 const img = document.createElement('img')
-const data = []
-data.push(fileUpload.value[0])
-const path = window.URL.createObjectURL(new Blob(data, {type: "image/jpeg"}))
-img.setAttribute("src", path)
+// const data = []
+// data.push(fileUpload.value[0])
+if (fileUpload) {
+  console.log(typeof fileUpload.value[0])
+  const path = URL.createObjectURL(fileUpload.value[0])
+  img.src = path
+}
 const ctx = c?.getContext("2d") 
 ctx?.drawImage(img, 0, 0, img.width, img.height)
 
 const publish = () => {
-  // console.log(typeof fileUpload.value[0])
+  console.log(typeof fileUpload.value[0])
 }
 
 </script>
