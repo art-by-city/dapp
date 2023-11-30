892bb002198672ac4f0824685fec664e99f208cd
Nopfed <robby.tsunami@gmail.com>
2023-11-30 15:20:32 -0500

    WIP: Following working now

--- components/FollowButton.vue
+++ components/FollowButton.vue
@@ -16,21 +16,30 @@ const props = defineProps<{ address: string, owner: string }>()
 const abc = useArtByCity()
 
 const onFollowClick = async () => {
-  let txid = await abc.connect().following.getOrCreate()
+  let txid
+  try{
+    txid = await abc.connect().following.getOrCreate()
+  }catch(error){
+    console.log("Error on getOrCreate follow:", error)
+  }
 
   console.log("txId from getOrCreate", txid)
 
-  // try {
-  //   await abc.connect().following.follow(props.address)
-  // } catch(error){
-  //   console.log(error)
-  // }
-  // try {
-  //   console.log("following", await abc.following.following(props.owner))
-  // } catch(error){
-  //   console.log(error)
-  // }
-  
-  
+  try {
+    await abc.connect().following.follow(props.address)
+  } catch(error){
+    console.log(error)
+  }
+  try {
+    console.log("following", await abc.following.following(props.owner))
+  } catch(error){
+    console.log(error)
+  }
 }
+
+// const alreadyFollowing = computed( async () => {
+//   const following = await abc.following.following(props.owner)
+
+//   return following.includes(props.address)
+// })
 </script>
