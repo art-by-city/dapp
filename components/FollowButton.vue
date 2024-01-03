76a03373a4281ccc08c5bfe57e421f4ef998215b
Nopfed <robby.tsunami@gmail.com>
2024-01-03 13:52:31 -0500

    Adds unfollow and loading logic

--- components/FollowButton.vue
+++ components/FollowButton.vue
@@ -1,55 +1,74 @@
 <template>
   <v-btn
-    v-if="!alreadyFollowing"
-    color="primary"
-    prepend-icon="mdi-plus"
     variant="outlined"
     density="compact"
     elevation="2"
+    :disabled="pending"
+    :loading="loading"
     @click="onFollowClick"
   >
-    Follow
-  </v-btn>
-
-  <v-btn
-    v-else
-    color="primary"
-    prepend-icon="mdi-minus"
-    variant="outlined"
-    density="compact"
-    elevation="2"
-    @click="onFollowClick"
-  >
-    Unfollow
+    <v-icon size="x-small">
+      {{ followActionIcon }}
+    </v-icon>
+    {{ followActionText }}
   </v-btn>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{ address: string, owner: string }>()
 const abc = useArtByCity()
+const loading = ref(false)
+
+const {
+  data: following,
+  pending,
+  refresh
+} = useLazyAsyncData(`follows-for-${props.owner}`, async () => {
+  const following = await abc.following.following(props.owner)
+
+  return following
+})
+
+const isFollowing = computed(() => {
+  if (!following.value) { return false }
+
+  return following.value.includes(props.address)
+})
+
+const followActionIcon = computed( () => {
+  return isFollowing.value
+    ? 'mdi-minus' : 'mdi-plus'
+})
+
+const followActionText = computed( () => {
+  return isFollowing.value
+    ? 'Unfollow' : 'Follow'
+})
 
 const onFollowClick = debounce(async () => {
+  loading.value = true
+
   try {
-    const txid = await abc.connect().following.getOrCreate()
-    console.log("txID from getOrCreate", txid)
+    await abc.connect().following.getOrCreate()
   }catch(error){
     console.log("Error on getOrCreate follow:", error)
   }
 
-  if (!alreadyFollowing) {
-    try { // follow
+  if (!isFollowing.value) {
+    try { // follow      
       await abc.connect().following.follow(props.address)
+      await refresh()
     }catch(error){
-      console.log(error)
+      console.log('Error when attempting to follow.', error)
     }
   }else{ // unfollow
-    console.log("Already following user?", await alreadyFollowing.value)
-  } 
-})
-
-const alreadyFollowing = computed( async () => {
-  const following = await abc.following.following(props.owner)
-
-  return following.includes(props.address)
+    try {
+      await abc.connect().following.unfollow(props.address)
+      await refresh()
+    }catch(error){
+      console.log('Error when attempting to unfollow.', error)
+    }
+  }
+  loading.value = false
 })
 </script>
